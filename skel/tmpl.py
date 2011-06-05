ca843a354919344bd77ee8ca2401bf77d541ab50
Dan F-M <dan@danfm.ca>
2011-06-04 22:23:18 -0400

    README/templates

---
+++ skel/tmpl.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+%FILENAME%
+
+Created by Dan Foreman-Mackey on %DATE%
+"""
+
+import numpy as np
+import pylab as pl
+
+
+def main():
+    %START%
+
+if __name__ == '__main__':
+    main()
+
